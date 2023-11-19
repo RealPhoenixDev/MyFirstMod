0d3f0f3ec32e57379e046a7cfe9e4ce2fbfcba79
PhoenixDev <147312817+RealPhoenixDev@users.noreply.github.com>
2023-11-19 20:08:22 +0200

    Initial commit

---
+++ src/main/java/net/phoenixdev/myfirstmod/MyFirstModClient.java
@@ -0,0 +1,8 @@
+package net.phoenixdev.myfirstmod;
+
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MyFirstModClient implements ClientModInitializer {
+    public void onInitializeClient() {}
+}
